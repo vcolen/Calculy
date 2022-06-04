52827a6b3673e46912be470c9cc07d9b6da7105b
VIctor <colen.victor@icloud.com>
2022-06-03 22:19:00 -0300

    Initial Commit

---
+++ Calculy/CalculyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculyApp.swift
+//  Calculy
+//
+//  Created by Victor Colen on 03/06/22.
+//
+
+import SwiftUI
+
+@main
+struct CalculyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
